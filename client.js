dac592dc2b07ce63a03d47234267c2293325959b
timotejroiko <timotejroiko@github.com>
2020-05-14 16:54:34 +0100

    fix unwanted caching of permissionOverwrites

--- client.js
+++ client.js
@@ -291,6 +291,27 @@ Discord.Channel.create = (client, data, guild) => {
 	return channel;
 }
 
+Discord.ChannelManager.prototype.add = function(data, guild, cache = true) {
+	if(!this.client.options.enablePermissions) {
+		if(!this.client.guilds.cache.get(data.guild_id) || !this.client.guilds.cache.get(data.guild_id).roles.cache.size) {
+			data.permission_overwrites = [];
+		}
+	}
+	const existing = this.cache.get(data.id);
+	if(existing) {
+		if (existing._patch && cache) existing._patch(data);
+		if (guild) guild.channels.add(existing);
+		return existing;
+	}
+	const channel = Discord.Channel.create(this.client, data, guild);
+	if(!channel) {
+		this.client.emit(Discord.Constants.Events.DEBUG, `Failed to find guild, or unknown type for channel ${data.id} ${data.type}`);
+		return null;
+	}
+	if(cache) { this.cache.set(channel.id, channel); }
+	return channel;
+}
+
 Discord.Client = class Client extends Discord.Client {
 	constructor(options = {}) {
 		options = Object.assign(
