b1bc8e5b2c8816b0bb1a82493810e35bdb50e266
Timotej Rojko <33236065+timotejroiko@users.noreply.github.com>
2020-08-31 23:04:35 +0100

    wip typings - add fetch methods

--- client.d.ts
+++ client.d.ts
@@ -6,21 +6,61 @@ export class Client extends Discord.Client {
 	public sweepChannels(lifetime: number): void;
 }
 
-type ChannelFetchOptions = { id?: Discord.Snowflake, cache?: boolean, withOverwrites?: boolean, force?: boolean }
+type ChannelFetchOptions = {
+	id?: Discord.Snowflake
+	cache?: boolean
+	withOverwrites?: boolean
+	force?: boolean
+}
+type GuildFetchOptions = {
+	id?: Discord.Snowflake
+	cache?: boolean
+	limit?: number
+	before?: Discord.Snowflake
+	after?: Discord.Snowflake
+	force?: boolean
+}
+type MemberFetchOptions = {
+	user?: Discord.Snowflake | Array<Discord.Snowflake>
+	cache?: boolean
+	rest?: boolean
+	query?: string
+	limit?: number
+	after?: Discord.Snowflake
+	withPresences?: boolean
+	time?: number
+	force?: boolean
+}
+type EmojiFetchOptions = {
+	id?: Discord.Snowflake
+	cache?: boolean
+	force?: boolean
+}
+type RoleFetchOptions = {
+	id?: Discord.Snowflake
+	cache?: boolean
+	force?: boolean
+}
+type ReactionUserFetchOptions = {
+	cache?: boolean
+	limit?: number
+	before?: Discord.Snowflake
+	after?: Discord.Snowflake
+}
 
 declare module "discord.js-light" {
 	interface ClientOptions {
-		cacheChannels?:boolean,
-		cacheGuilds?:boolean,
-		cachePresences?:boolean,
-		cacheRoles?:boolean,
-		cacheOverwrites?:boolean,
-		cacheEmojis?:boolean,
+		cacheChannels?:boolean
+		cacheGuilds?:boolean
+		cachePresences?:boolean
+		cacheRoles?:boolean
+		cacheOverwrites?:boolean
+		cacheEmojis?:boolean
 		disabledEvents?: Array<string>
 	}
 	interface ClientEvents {
-		rest:[{path:string,method:string,response:Promise<Buffer>}],
-		shardConnect:[number,Discord.Collection<Discord.Snowflake,Discord.Guild>],
+		rest:[{path:string,method:string,response:Promise<Buffer>}]
+		shardConnect:[number,Discord.Collection<Discord.Snowflake,Discord.Guild>]
 		guildEmojisUpdate:[Discord.Collection<Discord.Snowflake,Discord.GuildEmoji>]
 	}
 	interface UserManager {
@@ -28,8 +68,17 @@ declare module "discord.js-light" {
 	}
 	interface GuildManager {
 		forge(id: Discord.Snowflake): Discord.Guild
+		fetch(): Promise<Discord.Collection<Discord.Snowflake, Discord.Guild>>
+		fetch(id: Discord.Snowflake): Promise<Discord.Guild>
+		fetch(id: Discord.Snowflake, cache: boolean): Promise<Discord.Guild>
+		fetch(id: Discord.Snowflake, options: GuildFetchOptions): Promise<Discord.Guild>
+		fetch(cache: boolean): Promise<Discord.Collection<Discord.Snowflake, Discord.Guild>>
+		fetch(cache: boolean, options: GuildFetchOptions): Promise<Discord.Collection<Discord.Snowflake, Discord.Guild>>
+		fetch(options: { id: Discord.Snowflake } & GuildFetchOptions): Promise<Discord.Guild>
+		fetch(options: GuildFetchOptions): Promise<Discord.Collection<Discord.Snowflake, Discord.Guild>>
 	}
 	interface ChannelManager {
+		forge(id: Discord.Snowflake): Discord.DMChannel
 		forge(id: Discord.Snowflake, type?: "dm"): Discord.DMChannel
 		forge(id: Discord.Snowflake, type: "text"): Discord.TextChannel
 		forge(id: Discord.Snowflake, type: "voice"): Discord.VoiceChannel
@@ -37,24 +86,59 @@ declare module "discord.js-light" {
 		forge(id: Discord.Snowflake, type: "category"): Discord.CategoryChannel
 		forge(id: Discord.Snowflake, type: "news"): Discord.NewsChannel
 		forge(id: Discord.Snowflake, type: "store"): Discord.StoreChannel
-		fetch(idOrOptions: Discord.Snowflake | boolean | ChannelFetchOptions, cacheOrOptions?: boolean | ChannelFetchOptions) : Promise<Discord.Channel>
+		fetch(id: Discord.Snowflake): Promise<Discord.Channel>
+		fetch(id: Discord.Snowflake, cache: boolean): Promise<Discord.Channel>
+		fetch(id: Discord.Snowflake, options: ChannelFetchOptions): Promise<Discord.Channel>
+		fetch(options: { id: Discord.Snowflake } & ChannelFetchOptions): Promise<Discord.Channel>
 	}
 	interface GuildChannelManager {
-		forge(id: Discord.Snowflake, type?: "text"): Discord.TextChannel
+		forge(id: Discord.Snowflake): Discord.TextChannel
+		forge(id: Discord.Snowflake, type: "text"): Discord.TextChannel
 		forge(id: Discord.Snowflake, type: "voice"): Discord.VoiceChannel
 		forge(id: Discord.Snowflake, type: "category"): Discord.CategoryChannel
 		forge(id: Discord.Snowflake, type: "news"): Discord.NewsChannel
 		forge(id: Discord.Snowflake, type: "store"): Discord.StoreChannel
-		fetch(idOrCacheOrOptions?: Discord.Snowflake | boolean | ChannelFetchOptions, cacheOrOptions?: boolean | ChannelFetchOptions) : Promise<Discord.GuildChannel | Discord.Collection<Discord.Snowflake, Discord.GuildChannel>>
+		fetch(): Promise<Discord.Collection<Discord.Snowflake, Discord.GuildChannel>>
+		fetch(id: Discord.Snowflake): Promise<Discord.GuildChannel>
+		fetch(id: Discord.Snowflake, cache: boolean): Promise<Discord.GuildChannel>
+		fetch(id: Discord.Snowflake, options: ChannelFetchOptions): Promise<Discord.GuildChannel>
+		fetch(cache: boolean): Promise<Discord.Collection<Discord.Snowflake, Discord.GuildChannel>>
+		fetch(cache: boolean, options: ChannelFetchOptions): Promise<Discord.Collection<Discord.Snowflake, Discord.GuildChannel>>
+		fetch(options: { id: Discord.Snowflake } & ChannelFetchOptions): Promise<Discord.GuildChannel>
+		fetch(options: ChannelFetchOptions): Promise<Discord.Collection<Discord.Snowflake, Discord.GuildChannel>>
 	}
 	interface GuildMemberManager {
 		forge(id: Discord.Snowflake): Discord.GuildMemberManager
+		fetch(): Promise<Discord.Collection<Discord.Snowflake, Discord.GuildMember>>
+		fetch(user: Discord.Snowflake): Promise<Discord.GuildMember>
+		fetch(user: Discord.Snowflake, cache: boolean): Promise<Discord.GuildMember>
+		fetch(user: Discord.Snowflake, options: MemberFetchOptions): Promise<Discord.GuildMember>
+		fetch(cache: boolean): Promise<Discord.Collection<Discord.Snowflake, Discord.GuildMember>>
+		fetch(cache: boolean, options: MemberFetchOptions): Promise<Discord.Collection<Discord.Snowflake, Discord.GuildMember>>
+		fetch(options: { user: Discord.Snowflake } & MemberFetchOptions): Promise<Discord.GuildMember>
+		fetch(options: MemberFetchOptions): Promise<Discord.Collection<Discord.Snowflake, Discord.GuildMember>>
 	}
 	interface GuildEmojiManager {
 		forge(id: Discord.Snowflake): Discord.GuildEmojiManager
+		fetch(): Promise<Discord.Collection<Discord.Snowflake, Discord.GuildEmoji>>
+		fetch(id: Discord.Snowflake): Promise<Discord.GuildEmoji>
+		fetch(id: Discord.Snowflake, cache: boolean): Promise<Discord.GuildEmoji>
+		fetch(id: Discord.Snowflake, options: EmojiFetchOptions): Promise<Discord.GuildEmoji>
+		fetch(cache: boolean): Promise<Discord.Collection<Discord.Snowflake, Discord.GuildEmoji>>
+		fetch(cache: boolean, options: EmojiFetchOptions): Promise<Discord.Collection<Discord.Snowflake, Discord.GuildEmoji>>
+		fetch(options: { id: Discord.Snowflake } & EmojiFetchOptions): Promise<Discord.GuildEmoji>
+		fetch(options: EmojiFetchOptions): Promise<Discord.Collection<Discord.Snowflake, Discord.GuildEmoji>>
 	}
 	interface RoleManager {
 		forge(id: Discord.Snowflake): Discord.Role
+		fetch(): Promise<Discord.Collection<Discord.Snowflake, Discord.Role>>
+		fetch(id: Discord.Snowflake): Promise<Discord.Role>
+		fetch(id: Discord.Snowflake, cache: boolean): Promise<Discord.Role>
+		fetch(id: Discord.Snowflake, options: RoleFetchOptions): Promise<Discord.Role>
+		fetch(cache: boolean): Promise<Discord.Collection<Discord.Snowflake, Discord.Role>>
+		fetch(cache: boolean, options: RoleFetchOptions): Promise<Discord.Collection<Discord.Snowflake, Discord.Role>>
+		fetch(options: { id: Discord.Snowflake } & RoleFetchOptions): Promise<Discord.Role>
+		fetch(options: RoleFetchOptions): Promise<Discord.Collection<Discord.Snowflake, Discord.Role>>
 	}
 	interface MessageManager {
 		forge(id: Discord.Snowflake): Discord.Message
@@ -63,9 +147,11 @@ declare module "discord.js-light" {
 		forge(id: Discord.Snowflake): Discord.Presence
 	}
 	interface ReactionManager {
-		forge(id: Discord.Snowflake | string): Discord.MessageReaction
+		forge(id: Discord.Snowflake): Discord.MessageReaction
+		forge(unicodeEmoji: string): Discord.MessageReaction
 	}
 	interface ReactionUserManager {
-
+		fetch(): Promise<Discord.Collection<Discord.Snowflake, Discord.User>>
+		fetch(options: ReactionUserFetchOptions): Promise<Discord.Collection<Discord.Snowflake, Discord.User>>
 	}
 }
